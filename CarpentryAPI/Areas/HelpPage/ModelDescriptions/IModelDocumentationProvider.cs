25aaa577e314eb88d65a6a2699e0a7c7ddb444bd
Adalberto Velazquez <avbatista1985@gmail.com>
2020-04-12 23:22:54 -0400

    Add project files.

---
+++ CarpentryAPI/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace CarpentryAPI.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
---
+++ CarpentryAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CarpentryAPI.WebApiApplication" Language="C#" %>
